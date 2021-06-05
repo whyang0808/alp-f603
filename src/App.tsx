e3690d363e1092c5d47e8b228832f578be77a066
Choor Hong <abner.hong@hotmail.com>
2021-06-05 17:43:26 +0800

    Install additional packages & edit create-user componenets

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import NavBar from './App/components/layouts/NavBar'
+import CreateUser from './App/components/pages/auth/create-user'
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!
-        </p>
-      </header>
-    </div>
+    <NavBar>
+      <CreateUser />
+    </NavBar>
   )
 }
 
