742780dd73fc7ae0dc8ec6d057523ab0e8b9926e
Yang Wei Hao <whyang0808@gmail.com>
2021-06-20 18:06:45 +0800

    Clean up auth hook

--- src/App/shared/hooks/auth-hook.ts
+++ src/App/shared/hooks/auth-hook.ts
@@ -1,10 +1,7 @@
-import { useCallback, useEffect, useReducer } from 'react'
+import { useCallback, useReducer } from 'react'
 
 import { ROLE } from '../constants'
 import { IAuthState } from '../context/auth-context'
-
-const USER_DATA = 'userData'
-let logoutTimer: number
 
 const authInitialState: IAuthState = {
   token: undefined,
@@ -46,23 +43,15 @@ export const useAuth = () => {
     dispatch({
       type: 'login',
       payload: {
-        token: token,
+        token: `Bearer ${token}`,
         tokenExpirationDate: tokenExpirationDate,
         userId: uid,
         userRole: role
       }
     })
-
-    // localStorage.setItem(USER_DATA, JSON.stringify({ uid, role, token, expiration: tokenExpirationDate.toISOString() }))
   }, [])
 
-  const logout = useCallback(() => {
-    dispatch({
-      type: 'logout'
-    })
-
-    localStorage.removeItem(USER_DATA)
-  }, [])
+  const logout = useCallback(() => dispatch({ type: 'logout' }), [])
 
   const resetToken = useCallback<(token: string, expirationDate?: Date) => void>((token, expirationDate) => {
     // Set 1 hour expiration if expiration date is not given
@@ -77,24 +66,6 @@ export const useAuth = () => {
     })
   }, [])
 
-  // useEffect(() => {
-  //   const storedUserData = localStorage.getItem(USER_DATA)
-
-  //   if (storedUserData) {
-  //     const { uid, role, token, expiration } = JSON.parse(storedUserData)
-  //     if (new Date(expiration) > new Date()) {
-  //       login(uid, role, token, expiration)
-  //     }
-  //   }
-  // }, [login])
-
-  // useEffect(() => {
-  //   if (state.token && state.tokenExpirationDate) {
-  //     logoutTimer = window.setTimeout(logout, state.tokenExpirationDate.getTime() - new Date().getTime())
-  //   } else {
-  //     clearTimeout(logoutTimer)
-  //   }
-  // }, [logout, state.token, state.tokenExpirationDate])
   return {
     login,
     logout,
